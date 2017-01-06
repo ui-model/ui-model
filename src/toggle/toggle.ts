acd9c2d3afd0ed10647fc4d8ebb082068344efcf
Zhicheng Wang <zcwang@thoughtworks.com>
2017-01-06 12:34:37 +0800

    increase version number and publish

--- src/toggle/toggle.ts
+++ src/toggle/toggle.ts
@@ -12,16 +12,32 @@ export class Toggle extends Subject<boolean> {
     return this._on;
   }
 
+  set on(value: boolean) {
+    this._on = value;
+  }
+
   get off() {
     return !this.on;
+  }
+
+  set off(value: boolean) {
+    this.on = !value;
   }
 
   turnOn(): void {
     this.turnTo(true);
   }
 
+  open(): void {
+    this.turnOn();
+  }
+
   turnOff(): void {
     this.turnTo(false);
+  }
+
+  close(): void {
+    this.turnOff();
   }
 
   turnTo(state: boolean): void {
