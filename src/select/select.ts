731bc35124ffcfffce438865e855e93675f2ef05
Rex YE <rexebin@gmail.com>
2017-01-15 14:20:41 +0000

    select getter and setter

--- src/select/select.ts
+++ src/select/select.ts
@@ -1,20 +1,28 @@
-import {Supplier, Suppliers} from '../utils/supplier';
-import {Subject} from 'rxjs/Subject';
+import { Supplier, Suppliers } from '../utils/supplier';
+import { Subject } from 'rxjs/Subject';
 export class Select<T> extends Subject<T> {
-  selection: T;
-
+  _selection: T;
+  
+  get selection(): T {
+    return this._selection;
+  }
+  
+  set selection(item: T) {
+    this._selection = item;
+    this.next(item);
+  }
+  
   constructor(private supplier: Supplier<T, any> = Suppliers.objectById) {
     super();
   }
-
+  
   selected(item: T): boolean {
     return this.supplier(this.selection) === this.supplier(item);
   }
-
+  
   select(item: T): void {
     if (!this.selected(item)) {
       this.selection = item;
-      this.next(item);
     }
   }
 }
