4565e7644891e32babbcbe1f942df401ce393db9
Rex Ye <Rex Ye>
2017-01-20 13:55:49 +0000

    gitignore

--- src/select/select.ts
+++ src/select/select.ts
@@ -2,24 +2,24 @@ import { Supplier, Suppliers } from '../utils/supplier';
 import { Subject } from 'rxjs/Subject';
 export class Select<T> extends Subject<T> {
   _selection: T;
-  
+
   get selection(): T {
     return this._selection;
   }
-  
+
   set selection(item: T) {
     this._selection = item;
     this.next(item);
   }
-  
+
   constructor(private supplier: Supplier<T, any> = Suppliers.objectById) {
     super();
   }
-  
+
   selected(item: T): boolean {
     return this.supplier(this.selection) === this.supplier(item);
   }
-  
+
   select(item: T): void {
     if (!this.selected(item)) {
       this.selection = item;
