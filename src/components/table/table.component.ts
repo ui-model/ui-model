4b1fc6f632d6a55c12abf5372bcf0f9e324a25e3
Zhicheng Wang <zcwang@thoughtworks.com>
2017-02-13 14:26:46 +0800

    bugfix(pagination): Don't display pages if no record feat(table): add title for table cell feat(table): export items property

--- src/components/table/table.component.ts
+++ src/components/table/table.component.ts
@@ -1,13 +1,13 @@
-import {Component, OnInit, Input} from "@angular/core";
+import {Component} from "@angular/core";
 import {TableField, TableSorter} from "ui-model";
 
 @Component({
   selector: 'ui-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
-  inputs: ['fields', 'sortable'],
+  inputs: ['fields', 'items', 'sortable'],
 })
-export class TableComponent<T> implements OnInit {
+export class TableComponent<T> {
 
   _fields: TableField[];
   get fields(): TableField[] {
@@ -17,11 +17,11 @@ export class TableComponent<T> implements OnInit {
   set fields(value: TableField[]) {
     if (this._fields !== value) {
       this._fields = value;
-      this.sorter = new TableSorter(this._fields);
+      this.sorter.fields = value;
     }
   }
 
-  @Input() items: T[];
+  items: T[];
 
   get sortable(): boolean {
     return this.sorter.enabled;
@@ -31,14 +31,11 @@ export class TableComponent<T> implements OnInit {
     this.sorter.enabled = value;
   }
 
-  sorter: TableSorter;
+  sorter: TableSorter = new TableSorter([]);
 
   delta: {[field: string]: {x: number, y: number}} = {};
   size: {[field: string]: {x: number, y: number}} = {};
 
   constructor() {
-  }
-
-  ngOnInit() {
   }
 }
