d56af2356eefa7a509ef70620b12eed7cdeb8926
Zhicheng Wang <zcwang@thoughtworks.com>
2017-02-14 17:22:31 +0800

    feat(calendar): add event support and some tests

--- src/calendar/calendar.ts
+++ src/calendar/calendar.ts
@@ -1,13 +1,24 @@
-import * as moment from "moment";
+import * as moment from 'moment';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 
 type Moment = moment.Moment;
 type MomentInput = moment.MomentInput;
 
-const dow = moment.localeData().firstDayOfWeek();
-
 export class Calendar {
   constructor(value?: MomentInput) {
     this.value = value;
+  }
+
+  private _changes: Subject<Date> = new Subject<Date>();
+
+  protected changed(): void {
+    this.update();
+    this._changes.next(this._value.toDate());
+  }
+
+  get changes(): Observable<Date> {
+    return this._changes;
   }
 
   private _value: Moment = moment();
@@ -53,12 +64,12 @@ export class Calendar {
     return moment(date).isSame(new Date(), 'day');
   }
 
-  isPast(day: Date): boolean {
-    return this._value.isBefore(day);
+  isPast(date: Date): boolean {
+    return this._value.isAfter(date, 'day');
   }
 
   isFuture(date: Date): boolean {
-    return this._value.isAfter(date);
+    return this._value.isBefore(date, 'day');
   }
 
   inSameMonth(date: Date): boolean {
@@ -68,6 +79,33 @@ export class Calendar {
   isWeekEnd(date: Date): boolean {
     const weekday = date.getDay();
     return weekday === 0 || weekday === 6;
+  }
+
+  private _minValue: Moment;
+  get minValue(): MomentInput {
+    return this._minValue.toDate();
+  }
+
+  set minValue(value: MomentInput) {
+    this._minValue = moment(value);
+  }
+
+  private _maxValue: Moment;
+
+  get maxValue(): MomentInput {
+    return this._maxValue.toDate();
+  }
+
+  set maxValue(value: MomentInput) {
+    this._maxValue = moment(value);
+  }
+
+  isValid(date: MomentInput): boolean {
+    if (this._minValue && this._minValue.isSameOrBefore(date, 'day')) {
+      return true;
+    }
+    return !!(this._maxValue && this._maxValue.isSameOrAfter(date, 'day'));
+
   }
 
   goTo(date: Date): void {
@@ -82,11 +120,11 @@ export class Calendar {
     this.goTo(moment(this._value).add(step, 'month').toDate());
   }
 
-  prevMonth(step: number = 1): void {
+  goToPrevMonth(step: number = 1): void {
     this.addMonth(-step);
   }
 
-  nextMonth(step: number = 1): void {
+  goToNextMonth(step: number = 1): void {
     this.addMonth(step);
   }
 
@@ -99,12 +137,12 @@ export class Calendar {
   private _dates: Date[][] = [];
 
   dates(week: number): Date[] {
-    return this._dates[week];
+    return this._dates[week - this.weeks[0]];
   }
 
-  weekdays: ReadonlyArray<string> =  Object.freeze(moment.weekdaysMin(true));
+  readonly weekdays: string[] = moment.weekdaysMin(true);
 
-  months: ReadonlyArray<string> = Object.freeze(moment.months());
+  readonly months: string[] = moment.months();
 
   private _years: number[] = [];
 
@@ -112,29 +150,25 @@ export class Calendar {
     return this._years;
   }
 
-  changed(): void {
+  update(): void {
     const firstDayOfMonth = moment(this._value).startOf('month').local(true);
     const firstWeek = firstDayOfMonth.week();
-    const lastDayOfMonth = moment(this._value).endOf('month').local(true);
-
-    const lastWeek = lastDayOfMonth.week();
     const firstSunday = moment(firstDayOfMonth).startOf('week').local(true);
-    this._weeks = [];
-    this._dates = [];
-    for (let i = 0; i < 6; ++i) {
-      const week = firstWeek + i;
-      this._weeks.push(week);
-      this._dates[week] = [];
-      for (let j = 0; j < 7; ++j) {
-        this._dates[week].push(moment(firstSunday).add(i, 'week').add(j, 'day').toDate());
-      }
-      Object.freeze(this._dates[i]);
-    }
-    Object.freeze(this._weeks);
-    this._years = [];
-    for(let i = -5; i < 5; ++i) {
-      this._years.push(this.year + i);
-    }
-    Object.freeze(this._years);
+
+    this._weeks = range(firstWeek, firstWeek + 6);
+    this._dates = this.weeks.map((week) => {
+      return range(0, 7).map((day) => {
+        return moment(firstSunday).add(week - firstWeek, 'week').add(day, 'day').toDate();
+      });
+    });
+    this._years = range(this.year - 5, this.year + 6);
   }
+}
+
+function range(start: number, end: number): number[] {
+  const result = [];
+  for (let i = start; i < end; ++i) {
+    result.push(i);
+  }
+  return result;
 }
