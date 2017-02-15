e7dcdd48634d03c3d4422a967ad2b18812f982eb
Zhicheng Wang <zcwang@thoughtworks.com>
2017-02-15 10:32:42 +0800

    feat: add `moment` pipe

--- src/libraries/ui-model-angular/src/ui-model.module.ts
+++ src/libraries/ui-model-angular/src/ui-model.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SelectDirective } from './directives/select.directive';
-import { MultiSelectDirective } from './directives/multi-select.directive';
-import { ToggleDirective } from './directives/toggle.directive';
-import { PagerDirective } from './directives/pager.directive';
-import { PaginationDirective } from './directives/pagination.directive';
-import { ClickOutsideDirective } from './directives/click-outside.directive';
-import { ResizeDirective } from './directives/resize.directive';
-import { DraggableDirective } from './directives/draggable.directive';
-
-import { FormatPipe } from './pipes/format.pipe';
-import { OrderByPipe } from './pipes/order-by.pipe';
-import { OrderNamePipe } from './pipes/order-name.pipe';
-import { TypeNamePipe } from './pipes/type-name.pipe';
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {SelectDirective} from "./directives/select.directive";
+import {MultiSelectDirective} from "./directives/multi-select.directive";
+import {ToggleDirective} from "./directives/toggle.directive";
+import {PagerDirective} from "./directives/pager.directive";
+import {PaginationDirective} from "./directives/pagination.directive";
+import {ClickOutsideDirective} from "./directives/click-outside.directive";
+import {ResizeDirective} from "./directives/resize.directive";
+import {DraggableDirective} from "./directives/draggable.directive";
+import {FormatPipe} from "./pipes/format.pipe";
+import {OrderByPipe} from "./pipes/order-by.pipe";
+import {OrderNamePipe} from "./pipes/order-name.pipe";
+import {TypeNamePipe} from "./pipes/type-name.pipe";
+import {MomentPipe} from "./pipes/moment.pipe";
 
 @NgModule({
   imports: [
@@ -33,6 +33,7 @@ import { TypeNamePipe } from './pipes/type-name.pipe';
     OrderByPipe,
     OrderNamePipe,
     TypeNamePipe,
+    MomentPipe,
   ],
   exports: [
     SelectDirective,
@@ -49,6 +50,7 @@ import { TypeNamePipe } from './pipes/type-name.pipe';
     OrderByPipe,
     OrderNamePipe,
     TypeNamePipe,
+    MomentPipe,
   ],
 })
 export class UiModelModule {
@@ -69,3 +71,4 @@ export * from './pipes/format.pipe';
 export * from './pipes/order-by.pipe';
 export * from './pipes/order-name.pipe';
 export * from './pipes/type-name.pipe';
+export * from './pipes/moment.pipe';
