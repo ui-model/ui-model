d18922f5313e5912e9e08e414bc2bb1de2fc62d7
Zhicheng Wang <zcwang@thoughtworks.com>
2017-02-15 10:19:13 +0800

    chore: restructure

--- src/libraries/ui-model-angular/src/ui-model.module.ts
+++ src/libraries/ui-model-angular/src/ui-model.module.ts
@@ -5,14 +5,14 @@ import { MultiSelectDirective } from './directives/multi-select.directive';
 import { ToggleDirective } from './directives/toggle.directive';
 import { PagerDirective } from './directives/pager.directive';
 import { PaginationDirective } from './directives/pagination.directive';
+import { ClickOutsideDirective } from './directives/click-outside.directive';
+import { ResizeDirective } from './directives/resize.directive';
+import { DraggableDirective } from './directives/draggable.directive';
 
-import { ClickOutsideDirective } from './utils/click-outside.directive';
-import { ResizeDirective } from './utils/resize.directive';
-import { DraggableDirective } from './utils/draggable.directive';
-import { FormatPipe } from './utils/format.pipe';
-import { OrderByPipe } from './utils/order-by.pipe';
-import { OrderNamePipe } from './utils/order-name.pipe';
-import { TypeNamePipe } from './utils/type-name.pipe';
+import { FormatPipe } from './pipes/format.pipe';
+import { OrderByPipe } from './pipes/order-by.pipe';
+import { OrderNamePipe } from './pipes/order-name.pipe';
+import { TypeNamePipe } from './pipes/type-name.pipe';
 
 @NgModule({
   imports: [
@@ -62,10 +62,10 @@ export * from './directives/pager.directive';
 export * from './directives/pagination.directive';
 export * from './directives/multi-select.directive';
 
-export * from './utils/click-outside.directive';
-export * from './utils/draggable.directive';
-export * from './utils/resize.directive';
-export * from './utils/format.pipe';
-export * from './utils/order-by.pipe';
-export * from './utils/order-name.pipe';
-export * from './utils/type-name.pipe';
+export * from './directives/click-outside.directive';
+export * from './directives/draggable.directive';
+export * from './directives/resize.directive';
+export * from './pipes/format.pipe';
+export * from './pipes/order-by.pipe';
+export * from './pipes/order-name.pipe';
+export * from './pipes/type-name.pipe';
