54f1fb013930ba2da4a51e57d8830e77a1f39bd4
Zhicheng Wang <asnowwolf@gmail.com>
2017-03-05 22:16:56 +0800

    feat(pie-chart): add animations refactor: replace circle's cx/cy and text's x/y with transform style of their parent

--- src/app/showcase/chart/chart.component.ts
+++ src/app/showcase/chart/chart.component.ts
@@ -1,5 +1,5 @@
-import {Component} from "@angular/core";
-import {PieChart} from "ui-model";
+import {Component} from '@angular/core';
+import {PieChart, Pie} from 'ui-model';
 
 @Component({
   selector: 'ui-app-chart',
@@ -13,5 +13,10 @@ export class ChartComponent {
   noteOffset = 8;
 
   constructor() {
+  }
+
+  translateOf(pie: Pie): string {
+    const {x, y} = pie.mapToXY(pie.middlePercent, pie.radius + this.noteOffset);
+    return `translateX(${x}px) translateY(${y}px)`;
   }
 }
