460040cea8f48d0dde34568d0b6feae79899eef3
Zhicheng Wang <zcwang@thoughtworks.com>
2017-03-13 16:22:17 +0800

    feat(zoom): fix unit test

--- src/app/_libs/ui-model/src/zoom/zoom.spec.ts
+++ src/app/_libs/ui-model/src/zoom/zoom.spec.ts
@@ -7,8 +7,7 @@ describe('Zoom', () => {
     zoom.activeRect = Rect.from(0, 0, 100, 100);
     zoom.activeViewport = Rect.from(0, 0, 10, 10);
     zoom.passiveRect = Rect.from(0, 0, 1000, 1000);
-    zoom.changes.subscribe(() => {
-      expect(zoom.passiveViewport.width).to.eql(100);
-    });
+    zoom.update();
+    expect(zoom.passiveViewport.width).to.eql(100);
   });
 });
