22ad40bbddf7dff78059db6d58f957dcd91e4995
Zhicheng Wang <zcwang@thoughtworks.com>
2017-03-17 10:19:00 +0800

    feat: add more markdown plugins

--- src/app/shared/pipes/markdown.pipe.ts
+++ src/app/shared/pipes/markdown.pipe.ts
@@ -1,15 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as MarkdownIt from 'markdown-it';
 
+const markdown = new MarkdownIt();
+markdown.use(require('markdown-it-abbr'));
+markdown.use(require('markdown-it-deflist'));
+markdown.use(require('markdown-it-emoji'));
+markdown.use(require('markdown-it-footnote'));
+markdown.use(require('markdown-it-ins'));
+markdown.use(require('markdown-it-mark'));
+markdown.use(require('markdown-it-sub'));
+markdown.use(require('markdown-it-sup'));
+markdown.use(require('markdown-it-anchor'));
+markdown.use(require('markdown-it-table-of-contents'));
+
 @Pipe({
   name: 'markdown'
 })
 export class MarkdownPipe implements PipeTransform {
-  markdown = new MarkdownIt();
   transform(value: string): string {
     if (!value) {
       return '';
     }
-    return this.markdown.render(value);
+    return markdown.render(value);
   }
 }
