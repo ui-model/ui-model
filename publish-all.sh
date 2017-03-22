4bde3f6dba5ec6f374f8ad9754df8f55122bd7d3
Zhicheng Wang <zcwang@thoughtworks.com>
2017-03-22 12:17:12 +0800

    chore: increase version for publish

--- publish-all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd src/app/_libs/ui-model && npm publish && cd -
-cd src/app/_libs/ui-model-angular && npm publish && cd -
-cd src/app/_libs/ui-model-angular-bootstrap && npm publish && cd -
