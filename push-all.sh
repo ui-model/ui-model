3958ff61c08f338f15d58e3a297758aa2e0b16e0
Zhicheng Wang <zcwang@thoughtworks.com>
2017-03-23 13:32:11 +0800

    fix: conflict when subtree push

--- push-all.sh
+++ push-all.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git subtree push --prefix=src/app/_libs/ui-model ui-model master
-git subtree push --prefix=src/app/_libs/ui-model-angular ui-model-angular master
-git subtree push --prefix=src/app/_libs/ui-model-angular-bootstrap ui-model-angular-bootstrap master
+git push ui-model `git subtree split --prefix=src/app/_libs/ui-model master`:master
+git push ui-model-angular `git subtree split --prefix=src/app/_libs/ui-model-angular master`:master
+git push ui-model-angular-bootstrap `git subtree split --prefix=src/app/_libs/ui-model-angular-bootstrap master`:master
