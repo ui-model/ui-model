6809e9a5983fcd277571d275b67070e0d4ab6b71
Zhicheng Wang <zcwang@thoughtworks.com>
2017-05-26 09:14:12 +0800

    feat(forms): add FormArray to `reactive-form`

--- src/app/showcase/reactive-form/reactive-form.component.ts
+++ src/app/showcase/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { maxValue, minValue } from '@ui-model/angular';
 
 @Component({
@@ -14,18 +14,32 @@ export class ReactiveFormComponent implements OnInit {
       basic: fb.group({
         age: [0, Validators.compose([Validators.required, minValue(10), maxValue(20)])],
       }),
+      tags: fb.array([]),
     });
+    this.form.setValue({name: 'Tom', basic: {age: 10}, tags: []});
   }
 
-  form: FormGroup;
-
   get name(): AbstractControl {
-    return this.form && this.form.get('name');
+    return this.form.get('name');
   }
 
   get basic(): AbstractControl {
-    return this.form && this.form.get('basic');
+    return this.form.get('basic');
   }
+
+  get tags(): FormArray {
+    return this.form.get('tags') as FormArray;
+  }
+
+  newTag(): void {
+    this.tags.push(new FormControl(['']));
+  }
+
+  removeTag(i: number): void {
+    this.tags.removeAt(i);
+  }
+
+  form: FormGroup;
 
   ngOnInit(): void {
 
