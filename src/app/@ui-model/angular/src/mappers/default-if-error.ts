8e3d29c608b8ddb711b645f15e9742850dcae76e
Zhicheng Wang <asnowwolf@gmail.com>
2017-07-08 21:58:52 +0800

    fix: import `rxjs/add/observable/of`

--- src/app/@ui-model/angular/src/mappers/default-if-error.ts
+++ src/app/@ui-model/angular/src/mappers/default-if-error.ts
@@ -1,6 +1,7 @@
 import { Observable, ObservableInput } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 export function defaultIfError<T, R>(defaultValue?: R): (err: any, caught: Observable<T>) => ObservableInput<R> {
-  return function v(error: any, caught: Observable<T>): ObservableInput<R> {
+  return function v(): ObservableInput<R> {
     return Observable.of(defaultValue);
   };
 }
