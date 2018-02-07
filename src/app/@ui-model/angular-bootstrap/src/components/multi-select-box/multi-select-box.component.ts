e8b49156610d180f0e214acb9bd864d5de79bc9a
Zhicheng Wang <zcwang@thoughtworks.com>
2018-02-07 10:55:40 +0800

    fix: inline styleUrls for multi-select-box

--- src/app/@ui-model/angular-bootstrap/src/components/multi-select-box/multi-select-box.component.ts
+++ src/app/@ui-model/angular-bootstrap/src/components/multi-select-box/multi-select-box.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ui-multi-select-box',
   template: require('./multi-select-box.component.html'),
-  styleUrls: ['./multi-select-box.component.scss']
+  styles: [require('./multi-select-box.component.scss')],
 })
 export class MultiSelectBoxComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
