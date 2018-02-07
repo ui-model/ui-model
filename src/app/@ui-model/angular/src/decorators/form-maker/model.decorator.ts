f721cb9ad29beb9cbff12f45fa9ea5bc64faf14c
Zhicheng Wang <zcwang@thoughtworks.com>
2018-02-07 09:48:18 +0800

    feat: allow to use custom properties in ModelMetadata and PropertyMetadata

--- src/app/@ui-model/angular/src/decorators/form-maker/model.decorator.ts
+++ src/app/@ui-model/angular/src/decorators/form-maker/model.decorator.ts
@@ -1,6 +1,7 @@
 import { ModelMetadata } from './model-metadata';
 import { getOrCreateModelMetadata } from './utils';
-export function Model(metadata: ModelMetadata = {}): any {
+
+export function Model<T extends ModelMetadata>(metadata?: T): any {
   return function (target: any): void {
     const meta = getOrCreateModelMetadata(target);
     Object.assign(meta, metadata);
--- src/app/@ui-model/angular/src/decorators/form-maker/property.decorator.ts
+++ src/app/@ui-model/angular/src/decorators/form-maker/property.decorator.ts
@@ -1,6 +1,7 @@
 import { PropertyMetadata } from './property-metadata';
 import { getOrCreateProperty } from './utils';
-export function Property(metadata: PropertyMetadata = {}): any {
+
+export function Property<T extends PropertyMetadata>(metadata?: T): any {
   return function (target: any, name: string): void {
     const property = getOrCreateProperty(target, name);
     Object.assign(property, metadata);
