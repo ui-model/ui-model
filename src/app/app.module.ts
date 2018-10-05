6f1a3fc2a2cf68460a19426e3ada3d6becd4e274
Zhicheng Wang <asnowwolf@gmail.com>
2018-10-05 18:22:31 +0800

    fix: remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { UiModelModule } from '@ui-model/angular';
-import { UiModelBootstrapModule } from '@ui-model/angular-bootstrap';
-import { UiModelMarkdownModule } from '@ui-model/angular-markdown';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { AppComponent } from './layout/app/app.component';
 import { NavHeaderComponent } from './layout/nav-header/nav-header.component';
 import { NavMenuComponent } from './layout/nav-menu/nav-menu.component';
 import { SharedModule } from './shared/shared.module';
-import { UiModelD3Module } from '@ui-model/angular-d3';
 
 @NgModule({
   imports: [
@@ -18,10 +14,6 @@ import { UiModelD3Module } from '@ui-model/angular-d3';
     FormsModule,
     SharedModule,
     CoreModule,
-    UiModelModule,
-    UiModelBootstrapModule,
-    UiModelMarkdownModule,
-    UiModelD3Module,
     AppRoutingModule,
   ],
   declarations: [
