4325e5ede437c1ed6af92f0fdb681eb1b8e27146
Zhicheng WANG <zcwang@thoughtworks.com>
2019-07-24 11:53:12 +0800

    refactor: points 改名为 lines

--- projects/ui-model/chart/src/lib/utils/lines.spec.ts
+++ projects/ui-model/chart/src/lib/utils/lines.spec.ts
@@ -2,7 +2,7 @@ import { Point } from '@ui-model/core';
 import { lines } from './lines';
 
 describe('Line Chart', () => {
-  it('convert points to point pairs', () => {
+  it('convert lines to point pairs', () => {
     const data = [new Point(0, 1), new Point(1, 2), new Point(2, 0)];
     expect(lines(data)).toEqual([{ x1: 0, y1: 1, x2: 1, y2: 2 }, { x1: 1, y1: 2, x2: 2, y2: 0 }]);
   });
--- src/app/showcase/d3-line-chart/d3-line-chart.component.ts
+++ src/app/showcase/d3-line-chart/d3-line-chart.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { lines } from '@ui-model/chart';
+import { Line, lines } from '@ui-model/chart';
 import { Point } from '@ui-model/core';
 import { scaleLinear } from 'd3';
 
@@ -12,5 +12,5 @@ export class D3LineChartComponent {
   xScale = scaleLinear().domain([0, 6]).range([0, 1000]);
   yScale = scaleLinear().domain([0, 10]).range([0, 1000]);
 
-  points = lines([1, 5, 3, 10, 1, 0, 3].map((value, index) => new Point(this.xScale(index), this.yScale(value))));
+  lines: Line[] = lines([1, 5, 3, 10, 1, 0, 3].map((value, index) => new Point(this.xScale(index), this.yScale(value))));
 }
