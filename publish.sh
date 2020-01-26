e8274568a06bdb4e78e7a3c1c52054f4e0b9a2e0
Zhicheng WANG <zcwang@thoughtworks.com>
2020-01-26 22:06:38 +0800

    chore: 升级到新版本 fix: 去掉不用的库

--- publish.sh
+++ publish.sh
@@ -4,7 +4,6 @@ npm run build-libs
 npm publish --access public ./dist/ui-model/common/
 npm publish --access public ./dist/ui-model/core/
 npm publish --access public ./dist/ui-model/chart/
-npm publish --access public ./dist/ui-model/calendar/
 npm publish --access public ./dist/ui-model/angular/
 npm publish --access public ./dist/ui-model/angular-svg/
 npm publish --access public ./dist/ui-model/angular-d3/
